7e02ebcd2afdf3489f74f87f2d3a712588a49cf5
fabulouselephant <themostfabulouselephant@gmail.com>
2015-12-17 14:49:45 +0200

    wip

--- experiment.js
+++ experiment.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.sendfile("./experiment.html");
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
